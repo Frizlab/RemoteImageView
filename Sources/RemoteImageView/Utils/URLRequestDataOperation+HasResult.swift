67f286d12167342529c01a440bcda9b8141209d5
Frizlab <fload@me.com>
2022-06-19 18:25:28 +0200

    Update copyright year

--- Sources/RemoteImageView/Utils/URLRequestDataOperation+HasResult.swift
+++ Sources/RemoteImageView/Utils/URLRequestDataOperation+HasResult.swift
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 François Lamboley
+Copyright 2021-2022 François Lamboley
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
